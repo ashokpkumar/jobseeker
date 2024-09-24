c9298928fbcd66a39f4935d072bc1488e1a4c43d
Khaja-Banu <130048269+Khaja-Banu@users.noreply.github.com>
2024-09-24 22:04:13 +0530

    login api

--- jobseeker/finder/urls.py
+++ jobseeker/finder/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("example", views.index, name="index"),
-    path("adduser", views.insert_user, name="insert_user"),
-    path("new-user/<username>", views.new_user, name="new_user")
+    
+    path("login/", views.login, name="login")
 ]
--- jobseeker/finder/views.py
+++ jobseeker/finder/views.py
@@ -4,51 +4,50 @@ from datetime import datetime
 from rest_framework.decorators import api_view
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 @api_view(['POST'])
-def insert_user(request):
-    user_obj = User_table()
+def login(request): 
 
-    user_obj.user_id = 1234
-    user_obj.first_name = "nikhith"
-    user_obj.last_name = "raj"
-    user_obj.gender = 1
-    user_obj.nationality = "india"
-    user_obj.email = "nikhithtest@gmail.com"
-    user_obj.phone = "123456789"
-    user_obj.country_code = "91"
-    user_obj.user_type = 1
-    user_obj.created_by = 33
-    user_obj.created_time = datetime.now()
-    user_obj.modified_by = 32
-    user_obj.modified_time = datetime.now()
-    user_obj.hidden = False
+    username = request.data.get("username")
+    given_password = request.data.get("password")
+
+    try:
+        username = int(username)
     
-    user_obj.save()
-
-    return HttpResponse("Created a new user.")
-
-@api_view(['POST'])
-def new_user(request, username):
-    user_obj = User_table()
-
-    user_obj.user_id = 1234
-    user_obj.first_name = request.query_params.get("firstname")
-    user_obj.last_name = request.query_params.get("lastname")
-    user_obj.gender = request.query_params.get("gender")
-    user_obj.nationality = request.query_params.get("nationality")
-    user_obj.email = request.query_params.get("email")
-    user_obj.phone = request.query_params.get("phone")
-    user_obj.country_code = request.query_params.get("country_code")
-    user_obj.user_type = request.query_params.get("user_type")
-    user_obj.created_by = request.query_params.get("created_by")
-    user_obj.created_time = datetime.now()
-    user_obj.modified_by = request.query_params.get("modified_by")
-    user_obj.modified_time = datetime.now()
-    user_obj.hidden = True
+        user_obj = User_table.objects.filter(phone = username).first()
+        if user_obj is None:
+            return HttpResponse(f"{username} not available.")
+        if user_obj.password_hash == given_password:
+            return HttpResponse(f"{username} is valid.")
+        else:
+            return HttpResponse("Invalid Password.")
     
-    user_obj.save()
+    except:
+        
+        if username.find(".") > -1 and username.find("@") > -1:
+            user_obj = User_table.objects.filter(email = username).first()
+            if user_obj is None:
+                return HttpResponse(f"{username} not available.")
+            if user_obj.password_hash == given_password:
+                return HttpResponse(f"{username} is valid.")
+            else:
+                return HttpResponse("Invalid Password.")
+        else:
+            return HttpResponse("Invalid Username")
 
-    return HttpResponse("new user created.")
+
+    # if "@" and "." in given_username:
+    #     if given_username in User_table.email:
+    #         password_hash = hash(given_password)
+    #         if password_hash in User_table.password_hash:
+    #             return_message = f"Success. User {user_obj.first_name} {user_obj.last_name} is available."
+    #         else:
+    #             return_message = "Wrong password."
+    #     else:
+    #         return_message = "User not available."
+    # return return_message
+
+
+
+
+    user_obj.save()
+    return 
