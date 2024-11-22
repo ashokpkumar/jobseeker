a3f9e3cf7c7b17adfe8974133b89ccd38528426c
NikhithRajK <109682731+NikhithRaj-K@users.noreply.github.com>
2024-11-22 20:32:20 +0530

    redis_config

--- jobseeker/jobseeker/settings.py
+++ jobseeker/jobseeker/settings.py
@@ -224,3 +224,17 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1", 
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "jf_"
+    }
+}
